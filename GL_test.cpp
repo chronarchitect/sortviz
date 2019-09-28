6c3f889fb59943a2011a3bb9644f029ccf44128d
chronarchitect <aniketdas674@gmail.com>
2019-09-29 01:38:24 +0530

    Added array display as bar graph

--- GL_test.cpp
+++ GL_test.cpp
@@ -1,26 +1,40 @@
 #include <GL/gl.h>
 #include <GL/glut.h>
 #include <GL/glu.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <time.h>
 
-void setup() {   glClearColor(1.0f, 1.0f, 1.0f, 1.0f); }
+static int random_seed = 0;
+
+void setup() {   glClearColor(0.0f, 0.0f, 0.0f, 1.0f); }
 
 void display()
-   {
-      glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-      glColor3f(0.0f, 0.0f, 0.0f);
-      glRectf(-0.75f,0.75f, 0.75f, -0.75f);
-      glutSwapBuffers();
-   }
+{
+	srandom(random_seed);
+	float arr[200];
+	for ( int i = 0; i < 200; i++){
+		arr[i] = (float)(random() % 200)/100; 
+	}
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+	glColor3f(1.0f, 1.0f, 1.0f);
+	for (float i = -1; i < 1; i+=0.01){
+		glRectf(i, arr[(int)(i*100) + 100] -1, i+0.005, -1.0f);
+		glutSwapBuffers();
+	}
+}
 
 int main(int argc, char *argv[])
-  {
-     glutInit(&argc, argv);
-     glutInitDisplayMode(GLUT_RGB | GLUT_DEPTH | GLUT_DOUBLE);
-     glutInitWindowSize(800,600);
-     glutCreateWindow("Hello World");
+{
+	random_seed = random();
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_RGB | GLUT_DEPTH | GLUT_DOUBLE);
+	glutInitWindowSize(800,600);
+	glutCreateWindow("Hello World");
 
-     setup();
-     glutDisplayFunc(display);
-     glutMainLoop();
-     return 0;
-  }
+	setup();
+	glutDisplayFunc(display);
+	glutMainLoop();
+	return 0;
+}
